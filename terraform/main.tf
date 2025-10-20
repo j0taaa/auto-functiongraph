eac579140e4fe2a34610d39eee148b9d6147884d
Gabriel Jota <64752301+j0taaa@users.noreply.github.com>
2025-10-20 15:43:38 -0300

    Fix Terraform backend init with OBS

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,9 +11,10 @@ terraform {
     bucket                      = "tf-state-autofunctiongraph"
     key                         = "fgs/functions.tfstate"
     region                      = "sa-brazil-1"
-    endpoint                    = "https://obs.sa-brazil-1.myhuaweicloud.com"
+    endpoints                   = { s3 = "https://obs.sa-brazil-1.myhuaweicloud.com" }
     skip_credentials_validation = true
     skip_region_validation      = true
+    skip_metadata_api_check     = true
     force_path_style            = true
   }
 }
